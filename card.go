a8827ebb04a5b1c5c148e4e310afc818cdc4f0e7
Tom Allpress <tomallpress@hotmail.com>
2019-09-04 16:53:55 +0100

    add code so far

---
+++ card.go
@@ -0,0 +1,103 @@
+//go:generate stringer -type=Suit,Rank
+//https://gophercises.com/exercises/deck
+package deck
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+type Suit uint8
+
+const (
+	Spade Suit = iota
+	Diamond
+	Club
+	Heart
+	Joker
+)
+
+var suits = [...]Suit{Spade, Diamond, Club, Heart}
+
+type Rank uint8
+
+const (
+	_ Rank = iota
+	Ace
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+const (
+	minRank = Ace
+	maxRank = King
+)
+
+type Card struct {
+	Rank
+	Suit
+}
+
+func (c Card) String() string {
+	if c.Suit == Joker {
+		return c.Suit.String()
+	}
+	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
+}
+
+func New(opts ...func([]Card) []Card) []Card {
+	var cards []Card
+	for _, suit := range suits {
+		for rank := minRank; rank <= maxRank; rank++ {
+			cards = append(cards, Card{Suit: suit, Rank: rank})
+		}
+	}
+	for _, opt := range opts {
+		cards = opt(cards)
+	}
+	return cards
+}
+
+func DefaulSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+func Sort(less func(cards []Card) func(i, j int) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
+}
+
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return absRank(cards[i]) < absRank(cards[j])
+	}
+}
+
+func absRank(c Card) int {
+	return int(c.Suit)*int(maxRank) + int(c.Rank)
+}
+
+func Shuffle(cards []Card) []Card {
+	ret := make([]Card, len(cards))
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	perm := r.Perm(len(cards))
+	for i, j := range perm {
+		ret[i] = cards[j]
+	}
+	return ret
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package deck
+
+import (
+	"fmt"
+)
+
+func main() {
+	cards := New()
+	fmt.Println(cards[4])
+}
